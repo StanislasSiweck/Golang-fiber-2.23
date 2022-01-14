e16383d72447069977a339a36cb74e56732260d9
siweck <Sdsa2130>
2022-01-14 11:00:54 +0100

    User join Role && no return password

--- model/user.go
+++ model/user.go
@@ -9,13 +9,13 @@ type User struct {
 	Id uint `gorm:"primaryKey" json:"id" validate:"omitempty"`
 
 	//FK
-	RoleId uint `gorm:"not null" json:"role_id" validate:"omitempty"`
-	Role   Role `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	RoleId uint  `gorm:"not null" json:"role_id" validate:"omitempty"`
+	Role   *Role `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 
 	Firstname string `gorm:"not null; type:varchar(255)" json:"firstname" validate:"required,max=255"`
 	Lastname  string `gorm:"not null; type:varchar(255)" json:"lastname" validate:"required,max=255"`
 	Email     string `gorm:"not null; type:varbinary(255)" json:"email" validate:"required,max=255"`
-	Password  string `gorm:"not null; type:varchar(255)" json:"password" validate:"required,max=255"`
+	Password  string `gorm:"not null; type:varchar(255)" json:"-" validate:"required,max=255"`
 
 	//Timestamps
 	CreatedAt time.Time      `json:"created_at"`
